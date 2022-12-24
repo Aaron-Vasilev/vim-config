5dac2eeddbd527f09124b32e2a7bc0eb1e7bd388
Aaron Vasilev <oigen711@gmail.com>
2022-12-24 12:20:14 +0200

    Added rest and harpoon remap

---
+++ after/plugin/rest.lua
@@ -0,0 +1,7 @@
+local restNvim = require("rest-nvim")
+
+restNvim.setup()
+
+function rest()
+  restNvim.run()
+end
--- lua/my/packer.lua
+++ lua/my/packer.lua
@@ -2,16 +2,14 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  
+
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-  
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}) 
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
@@ -40,6 +38,14 @@ return require('packer').startup(function(use)
   }
 
   use ({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+               vim.cmd('colorscheme rose-pine')
+             end
+  })
+
+  use ({
     'sonph/onehalf',
     rtp = 'vim',
     config = function()
@@ -47,12 +53,40 @@ return require('packer').startup(function(use)
              end
   })
 
-  use ({
-    'rose-pine/neovim',
-    as = 'rose-pine',
+  use {
+    "rest-nvim/rest.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
     config = function()
-               vim.cmd('colorscheme rose-pine')
-             end
-  })
+      require("rest-nvim").setup({
+        result_split_horizontal = false,
+        result_split_in_place = false,
+        skip_ssl_verification = false,
+        -- Encode URL before making request
+        encode_url = true,
+        highlight = {
+          enabled = true,
+          timeout = 150,
+        },
+        result = {
+          show_url = true,
+          show_http_info = true,
+          show_headers = true,
+          -- executables or functions for formatting response body [optional]
+          -- set them to nil if you want to disable them
+          formatters = {
+            json = "jq",
+            html = function(body)
+              return vim.fn.system({"tidy", "-i", "-q", "-"}, body)
+            end
+          },
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        env_file = '.env',
+        custom_dynamic_variables = {},
+        yank_dry_run = true,
+      })
+    end
+  }
 
 end)
